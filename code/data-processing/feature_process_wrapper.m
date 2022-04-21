e417bc089614f99f3f1bc73db3c520be9d4b99d3
William Plucknett <70236773+Wilhamy@users.noreply.github.com>
2022-04-20 21:45:40 -0400

    augments test and train
    augmetns them separately

--- code/data-processing/feature_process_wrapper.m
+++ code/data-processing/feature_process_wrapper.m
@@ -1,10 +1,10 @@
 % automate process
 clear;
 ROOT = "D:\thewi\Documents\UM\WN22\ML\Project\Datasets\ml-project\";
-DATADIR = ROOT + "original\";
+DATADIR = ROOT + "solo\";
 OUTDIR = ROOT + "aug\";
 files = dir(DATADIR + "\*.mat");
-extractor = @raw_process ; % feature extractor
+extractor = @augmented_raw_process ; % feature extractor
 
 for idx = 1:size(files,1)
     filename = files(idx).name;
---
+++ code/liltests/examine_data.m
@@ -0,0 +1,3 @@
+load D:\thewi\Documents\UM\WN22\ML\Project\Datasets\ml-project\aug\AUG_5F-SubjectC-151204-5St-SGLHand.mat
+
+% x1 = data.examples(data.labels==1,:,4);
