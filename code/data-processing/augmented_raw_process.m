5f5fe97f796defce95f5fdc2bd91f319fefb5538
dabraham24 <drhombus24@gmail.com>
2022-04-20 20:13:08 -0400

    changes to augmented_raw_process to allow for data augmentation

--- code/data-processing/augmented_raw_process.m
+++ code/data-processing/augmented_raw_process.m
@@ -4,10 +4,11 @@
 %       filename: name of .mat file
 % Outputs: Saves file in .mat located at OUTDIR\FTA_filename
 % Note: the examples are sorted by label.
-function [] = augmented_raw_process(DATADIR, OUTDIR, filename)
+function [] = raw_process(DATADIR, OUTDIR, filename)
 
 load(DATADIR + filename) % loads into o
 
+offset = [-0.1, 0, 0.1, 0.5];
 d = [0;diff(o.marker)];
 d(d < 0) = 0;
 d(d > 5) = 0;
@@ -15,25 +16,24 @@ d(d > 5) = 0;
 [uniqu_d, cnt_uniq']
 locs = find(d); % indexes of the onsets
 
-onset_win_s = [0, 0.85]; % seconds relative to onset
-onset_win_n = onset_win_s * o.sampFreq; % number of datapoints relative to onset
-
-win_len = diff(onset_win_n); % length of window, in samples
-
-rel_channels = [1:21]; %relevant channels, removes X3
+rel_channels = [1:21]; %relevant channels, removes A1, A2, X3
 rel_chnames = o.chnames(rel_channels); % relevant channel names
-
 examples = [];
 labels = [];
-for idx = 1:length(locs)
-    loc = locs(idx);
-    win = onset_win_n + loc;
-    
-    ex = o.data(win(1):win(2)-1,rel_channels);
-    
-    examples(end+1, :, :) = ex;
-    labels(end+1,:) = d(loc);
+
+for off = offset
+    onset_win_s = [0, 0.85] + offset;
+    onset_win_n = onset_win_s * o.sampFreq; % number of datapoints relative to onset
+    for idx = 1:length(locs)
+        loc = locs(idx);
+        win = onset_win_n + loc;
+        ex = o.data(win(1):win(2)-1,rel_channels);
+        examples(end+1, :, :) = ex;
+        labels(end+1,:) = d(loc);
+    end
 end
+% win_len = diff(onset_win_n); % length of window, in samples
+
 
 [labels_sort, sort_idx] = sort(labels);
 examples_sort = examples(sort_idx,:,:);
